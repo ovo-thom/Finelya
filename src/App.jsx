d815b64f3109820cc4e8f11f0e9d505cf51b6177
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-14 16:30:37 +0200

    feat: add UX/UI Sidebar.jsx, responsive and add react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gray-200">
     <Sidebar />
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,30 @@
+import { MdSpaceDashboard } from "react-icons/md";
+import { HiOutlineDocumentText } from "react-icons/hi";
+import { TbSettings } from "react-icons/tb";
+
 export default function Sidebar() {
   return (
-    <aside className="h-screen w-72 bg-amber-500">Sidebar</aside>
-  )
+    <aside className="h-screen w-20 md:w-72 bg-gradient-to-b from-[#4b2bc2] via-[#7e21c9] to-[#9f1dd9]">
+      <ul className="text-gray-100 w-3/4 mx-auto py-12">
+        <li className="flex items-center px-4 rounded-xl mb-5 py-2 focus:bg-white/20 hover:bg-white/10 cursor-pointer">
+          <span className="mr-3">
+            <MdSpaceDashboard className="text-2xl md:text-lg"/>
+          </span>
+          <span className="hidden md:inline">Tableau de bord</span>
+        </li>
+        <li className="flex items-center px-4 rounded-xl mb-5 py-2 focus:bg-white/20 hover:bg-white/10 cursor-pointer">
+          <span className="mr-3">
+            <HiOutlineDocumentText className="text-2xl md:text-lg"/>
+          </span>
+          <span className="hidden md:inline">Transactions</span>
+        </li>
+        <li className="flex items-center px-4 rounded-xl mb-5 py-2 focus:bg-white/20 hover:bg-white/10 cursor-pointer">
+          <span className="mr-3">
+            <TbSettings className="text-2xl md:text-lg"/>
+          </span>
+          <span className="hidden md:inline">Paramètres</span>
+        </li>
+      </ul>
+    </aside>
+  );
 }
-
-// dessus : 4b2bc2
-// milieu : 7e21c9
-// bas : 9f1dd9

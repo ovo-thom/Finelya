d9adf5049632205b2de6d034f8cdd8f45f741b87
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-14 15:02:12 +0200

    feat(create): Finelya, tailwind and first component Sidebar.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "./components/Sidebar";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div>
+    <Sidebar />
+    </div>
+  )
+  
+    
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+    <aside className="h-screen w-72 bg-amber-500">Sidebar</aside>
+  )
+}
+
+// dessus : 4b2bc2
+// milieu : 7e21c9
+// bas : 9f1dd9

2fcf41ad51b6b709568ef30f96f217d8257e3836
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-14 16:43:50 +0200

    feat: add AddTransactionButton.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import AddTransactionButton from "./components/AddTransactionButton";
 import Sidebar from "./components/Sidebar";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
   return (
-    <div className="bg-gray-200">
-    <Sidebar />
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1 p-8">
+        <div className="flex justify-end mb-8">
+          <AddTransactionButton />
+        </div>
+      </div>
     </div>
-  )
-  
-    
+  );
 }
 
 export default App;
---
+++ src/components/AddTransactionButton.jsx
@@ -0,0 +1,5 @@
+export default function AddTransactionButton() {
+  return (
+    <button className="bg-green-700 text-gray-50 px-4 py-2 rounded-lg cursor-pointer">Ajouter dépense/transaction</button>
+  )
+}

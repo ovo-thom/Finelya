1f528047c12acfd539613d3b565729f29ac391cc
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-16 15:27:40 +0200

    feat: create TransactionsPage.jsx

---
+++ src/TransactionsPage.jsx
@@ -0,0 +1,9 @@
+import TransactionsTable from "./components/Dashboard/TransactionsTable";
+
+export default function TransactionsPage() {
+  return (
+    <div>
+        <TransactionsTable />
+    </div>
+  )
+}
